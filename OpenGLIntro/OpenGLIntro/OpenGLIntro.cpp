b732f069795e7487d2924a84bd52fea142ac6a73
shahebannis <74209018+shahebannis@users.noreply.github.com>
2024-07-29 21:21:36 -0400

    Added the dependencies and started the assignment
    Sorted the opengl dependencies and managed to make a window appear without the triangle.

---
+++ OpenGLIntro/OpenGLIntro/OpenGLIntro.cpp
@@ -0,0 +1,132 @@
+// OpenGLIntro.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+//Define Vertex and Fragment Shader
+const char* vertexShaderSource = R"glsl(
+        #version 330 core
+        layout (location = 0) in vec3 aPos;
+        uniform mat4 tranform;
+        void main() {
+            gl_Position = tranform * vec4(aPos, 1.0);
+        }
+    )glsl";
+
+const char* fragmentShaderSource = R"glsl(
+    #version 330 core
+    out vec4 FragColor;
+    void main() {
+        FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+    }
+)glsl";
+
+//Define vertices for a triangle
+float verticesTriangle[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+};
+
+unsigned int vertexShader, fragmentShader, shaderProgram;
+
+void compileShaders() {
+    //vertext shader
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    //Fragment Shader
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    //Link shaders into a program
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    //Clean up shaders as they're linked into our program now
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+}
+
+unsigned int VBO[2], VAO[2], EBO;
+void setupBuffers() {
+    //setup the VAO, VBO and EBO
+    glGenVertexArrays(2, VAO);
+    glGenBuffers(2, VBO);
+    glGenBuffers(1, &EBO);
+
+    //Bind the VAO for the triangle
+    glBindVertexArray(VAO[0]);
+}
+
+int main()
+{
+
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Triangle", NULL, NULL);
+
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    //setup Buffers
+    compileShaders();
+    setupBuffers();
+
+    //render the shapes
+    //render loop
+    while (!glfwWindowShouldClose(window)) {
+        //processInput(window);
+
+        //rendering commands
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO[0]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Cleanup
+    glDeleteVertexArrays(2, VAO);
+    glDeleteBuffers(2, VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
